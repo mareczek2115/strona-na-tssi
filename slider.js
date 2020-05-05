2538f25f053522ab8175793f107785192c919900
mareczek2115 <markoni@spoko.pl>
2020-05-05 16:47:46 +0200

    add slider.js and other small changes

--- slider.js
+++ slider.js
@@ -1,7 +1,89 @@
-function setDotColor(){
-    document.getElementsById("one").style.backgroundColor = "#a695a5";
+var image = "one";
+hoveredDot = document.getElementsByClassName("dot");
+
+hoveredDot.addEventListener('mouseenter', e =>{
+    hoveredDot.style.backgroundColor = "#a695a5";
+});
+hoveredDot.addEventListener('mouseleave', e =>{
+    hoveredDot.style.backgroundColor = "#fff8ef";
+});
+
+function setDotColor(){  
+    document.getElementById("one").style.backgroundColor = "#a695a5";
 }
 
 function changeImage(id){
-    document.getElementsById("slider").setAttribute("src", "");
+    if(id == "next"){
+        switch(image){
+            case "one":
+                document.getElementById("slider").setAttribute("src", "image2.png");
+                document.getElementById("one").style.backgroundColor = "#fff8ef";
+                document.getElementById("two").style.backgroundColor = "#a695a5";
+                image = "two";
+                break;
+            case "two":
+                document.getElementById("slider").setAttribute("src", "image3.png");
+                document.getElementById("two").style.backgroundColor = "#fff8ef";
+                document.getElementById("three").style.backgroundColor = "#a695a5";
+                image = "three";
+                break;
+            case "three":
+                document.getElementById("slider").setAttribute("src", "image4.png");
+                document.getElementById("three").style.backgroundColor = "#fff8ef";
+                document.getElementById("four").style.backgroundColor = "#a695a5";
+                image = "four";
+                break;
+            case "four":
+                document.getElementById("slider").setAttribute("src", "image5.png");
+                document.getElementById("four").style.backgroundColor = "#fff8ef";
+                document.getElementById("five").style.backgroundColor = "#a695a5";
+                image = "five";
+                break;
+            case "five":
+                document.getElementById("slider").setAttribute("src", "image1.png");
+                document.getElementById("five").style.backgroundColor = "#fff8ef";
+                document.getElementById("one").style.backgroundColor = "#a695a5";
+                image = "one";
+                break;
+        }
+    }
+    else{
+        switch(image){
+            case "one":
+                document.getElementById("slider").setAttribute("src", "image5.png");
+                document.getElementById("one").style.backgroundColor = "#fff8ef";
+                document.getElementById("five").style.backgroundColor = "#a695a5";
+                image = "five";
+                break;
+            case "two":
+                document.getElementById("slider").setAttribute("src", "image1.png");
+                document.getElementById("two").style.backgroundColor = "#fff8ef";
+                document.getElementById("one").style.backgroundColor = "#a695a5";
+                image = "one";
+                break;
+            case "three":
+                document.getElementById("slider").setAttribute("src", "image2.png");
+                document.getElementById("three").style.backgroundColor = "#fff8ef";
+                document.getElementById("two").style.backgroundColor = "#a695a5";
+                image = "two"
+                break;
+            case "four":
+                document.getElementById("slider").setAttribute("src", "image3.png");
+                document.getElementById("four").style.backgroundColor = "#fff8ef";
+                document.getElementById("three").style.backgroundColor = "#a695a5";
+                image = "free";
+                break;
+            case "five":
+                document.getElementById("slider").setAttribute("src", "image4.png");
+                document.getElementById("five").style.backgroundColor = "#fff8ef";
+                document.getElementById("four").style.backgroundColor = "#a695a5";
+                image = "four";
+                break;
+        }
+    }
+    
+}
+
+function dotChange(id){
+    
 }
