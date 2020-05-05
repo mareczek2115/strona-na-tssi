bcc55dd572a857ebc0358bb40fa3df7ca030245b
mareczek2115 <markoni@spoko.pl>
2020-05-05 14:07:40 +0200

    some changes in slider

---
+++ slider.js
@@ -0,0 +1,7 @@
+function setDotColor(){
+    document.getElementsById("one").style.backgroundColor = "#a695a5";
+}
+
+function changeImage(id){
+    document.getElementsById("slider").setAttribute("src", "");
+}
