17aebf577d2e9ec6a83faec04979c05d46814b1e
mareczek2115 <markoni@spoko.pl>
2020-05-06 19:16:41 +0200

    auto slides change

--- slider.js
+++ slider.js
@@ -1,8 +1,9 @@
 var image = "one";
+var number = 1;
 document.getElementById("one").style.backgroundColor = "#a695a5";
-document.querySelector("#photo").setAttribute("src", "images/image1.png");
 
 function changeImage(id){
+    clearTimeout(t);
     if(id == "next"){
         switch(image){
             case "one":
@@ -71,7 +72,7 @@ function changeImage(id){
                 break;
         }
     }
-    
+    x = setTimeout("autoChangeImage()", 5000);
 }
 
 function clearDots(){
@@ -110,42 +111,52 @@ function clearDots(){
 }
 
 function dotChange(id){
+    clearTimeout(t);
     dot = id.toString();
     switch(dot){
         case "one":
             document.getElementById("photo").setAttribute("src", "images/image1.png");
             document.querySelector("#one").style.backgroundColor = "#a695a5";
+            image = "one";
             clearDots();
             break;
         case "two":
             document.getElementById("photo").setAttribute("src", "images/image2.png");
             document.querySelector("#two").style.backgroundColor = "#a695a5";
+            image = "two";
             clearDots();
             break;
         case "three":
             document.getElementById("photo").setAttribute("src", "images/image3.png");
             document.querySelector("#three").style.backgroundColor = "#a695a5";
+            image = "three";
             clearDots();
             break;
         case "four":
             document.getElementById("photo").setAttribute("src", "images/image4.png");
             document.querySelector("#four").style.backgroundColor = "#a695a5";
+            image = "four";
             clearDots();
             break;
         case "five":
             document.getElementById("photo").setAttribute("src", "images/image5.png");
             document.querySelector("#five").style.backgroundColor = "#a695a5";
+            image = "five";
             clearDots();
             break;
     }
+    y = setTimeout("autoChangeImage()", 5000);
 }
 
 function autoChangeImage(){
-    while(true){
-        for(i=1; i<=5; i++){
-            n = i.toString();
-            console.log(`image${n}.png`);
-            setTimeout(4000);
-        }
+    if(number == 1){
+        document.querySelector("#photo").setAttribute("src", "images/image1.png");
     }
+    else{
+        changeImage("next");
+    }
+    number++;
+    t = setTimeout("autoChangeImage()", 5000);
+    clearTimeout(x);
+    clearTimeout(y);
 }
