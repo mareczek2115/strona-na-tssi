99a3fa371f7ec947394ff8019464983679aac58c
mareczek2115 <markoni@spoko.pl>
2020-05-06 17:02:55 +0200

    add images and something changed in slider.js

--- slider.js
+++ slider.js
@@ -1,35 +1,36 @@
 var image = "one";
 document.getElementById("one").style.backgroundColor = "#a695a5";
+document.querySelector("#photo").setAttribute("src", "images/image1.png");
 
 function changeImage(id){
     if(id == "next"){
         switch(image){
             case "one":
-                document.getElementById("photo").setAttribute("src", "image2.png");
+                document.getElementById("photo").setAttribute("src", "images/image2.png");
                 document.getElementById("one").style.backgroundColor = "#fff8ef";
                 document.getElementById("two").style.backgroundColor = "#a695a5";
                 image = "two";
                 break;
             case "two":
-                document.getElementById("photo").setAttribute("src", "image3.png");
+                document.getElementById("photo").setAttribute("src", "images/image3.png");
                 document.getElementById("two").style.backgroundColor = "#fff8ef";
                 document.getElementById("three").style.backgroundColor = "#a695a5";
                 image = "three";
                 break;
             case "three":
-                document.getElementById("photo").setAttribute("src", "image4.png");
+                document.getElementById("photo").setAttribute("src", "images/image4.png");
                 document.getElementById("three").style.backgroundColor = "#fff8ef";
                 document.getElementById("four").style.backgroundColor = "#a695a5";
                 image = "four";
                 break;
             case "four":
-                document.getElementById("photo").setAttribute("src", "image5.png");
+                document.getElementById("photo").setAttribute("src", "images/image5.png");
                 document.getElementById("four").style.backgroundColor = "#fff8ef";
                 document.getElementById("five").style.backgroundColor = "#a695a5";
                 image = "five";
                 break;
             case "five":
-                document.getElementById("photo").setAttribute("src", "image1.png");
+                document.getElementById("photo").setAttribute("src", "images/image1.png");
                 document.getElementById("five").style.backgroundColor = "#fff8ef";
                 document.getElementById("one").style.backgroundColor = "#a695a5";
                 image = "one";
@@ -39,31 +40,31 @@ function changeImage(id){
     else{
         switch(image){
             case "one":
-                document.getElementById("photo").setAttribute("src", "image5.png");
+                document.getElementById("photo").setAttribute("src", "images/image5.png");
                 document.getElementById("one").style.backgroundColor = "#fff8ef";
                 document.getElementById("five").style.backgroundColor = "#a695a5";
                 image = "five";
                 break;
             case "two":
-                document.getElementById("photo").setAttribute("src", "image1.png");
+                document.getElementById("photo").setAttribute("src", "images/image1.png");
                 document.getElementById("two").style.backgroundColor = "#fff8ef";
                 document.getElementById("one").style.backgroundColor = "#a695a5";
                 image = "one";
                 break;
             case "three":
-                document.getElementById("photo").setAttribute("src", "image2.png");
+                document.getElementById("photo").setAttribute("src", "images/image2.png");
                 document.getElementById("three").style.backgroundColor = "#fff8ef";
                 document.getElementById("two").style.backgroundColor = "#a695a5";
                 image = "two"
                 break;
             case "four":
-                document.getElementById("photo").setAttribute("src", "image3.png");
+                document.getElementById("photo").setAttribute("src", "images/image3.png");
                 document.getElementById("four").style.backgroundColor = "#fff8ef";
                 document.getElementById("three").style.backgroundColor = "#a695a5";
                 image = "three";
                 break;
             case "five":
-                document.getElementById("photo").setAttribute("src", "image4.png");
+                document.getElementById("photo").setAttribute("src", "images/image4.png");
                 document.getElementById("five").style.backgroundColor = "#fff8ef";
                 document.getElementById("four").style.backgroundColor = "#a695a5";
                 image = "four";
@@ -112,29 +113,39 @@ function dotChange(id){
     dot = id.toString();
     switch(dot){
         case "one":
-            document.getElementById("photo").setAttribute("src", "image1.png");
+            document.getElementById("photo").setAttribute("src", "images/image1.png");
             document.querySelector("#one").style.backgroundColor = "#a695a5";
             clearDots();
             break;
         case "two":
-            document.getElementById("photo").setAttribute("src", "image2.png");
+            document.getElementById("photo").setAttribute("src", "images/image2.png");
             document.querySelector("#two").style.backgroundColor = "#a695a5";
             clearDots();
             break;
         case "three":
-            document.getElementById("photo").setAttribute("src", "image3.png");
+            document.getElementById("photo").setAttribute("src", "images/image3.png");
             document.querySelector("#three").style.backgroundColor = "#a695a5";
             clearDots();
             break;
         case "four":
-            document.getElementById("photo").setAttribute("src", "image4.png");
+            document.getElementById("photo").setAttribute("src", "images/image4.png");
             document.querySelector("#four").style.backgroundColor = "#a695a5";
             clearDots();
             break;
         case "five":
-            document.getElementById("photo").setAttribute("src", "image5.png");
+            document.getElementById("photo").setAttribute("src", "images/image5.png");
             document.querySelector("#five").style.backgroundColor = "#a695a5";
             clearDots();
             break;
+    }
+}
+
+function autoChangeImage(){
+    while(true){
+        for(i=1; i<=5; i++){
+            n = i.toString();
+            console.log(`image${n}.png`);
+            setTimeout(4000);
+        }
     }
 }
